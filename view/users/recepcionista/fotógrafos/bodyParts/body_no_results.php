8fd0f516affc5c09ce04b277ad337b354de2086c
Rubén <rubencayuelasperez@gmail.com>
2024-04-05 16:46:51 +0200

    remove items

--- view/users/recepcionista/fotógrafos/bodyParts/body_no_results.php
+++
@@ -1,6 +0,0 @@
-      <div class="no_results">
-        <p class="text-center">No hay resultados</p>
-      </div>
-    </section>
-  </main>
-</body>
--- view/users/recepcionista/recepcionista_end.php
+++
@@ -1,6 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js">//JS de bootstrap.</script>
-<script src="../../../model/js/deplegar_menu/script.js">//JS controlador del menú desplegable.</script>
-<script src="../../../model/js/theme/script.js">// Script para el tema.</script>
-<script src="../../../model/js/collapse/script.js">// Script para cerrar los colapse si se abre otro.</script>
-
-</html>
