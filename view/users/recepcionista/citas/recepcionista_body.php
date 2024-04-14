15fa0b131f40e4f00df6e04e476a95677dc295ab
RubenCayu <rubencayuelasperez@gmail.com>
2024-04-14 19:19:06 +0200

    Theme fixes and button added

--- view/users/recepcionista/citas/recepcionista_body.php
+++ view/users/recepcionista/citas/recepcionista_body.php
@@ -14,39 +14,79 @@
     <script src="../../../model/js/recepcionista_calendar/script_prev.js">// Script prev</script>
 
     <nav>
-      <div class="wrapper wrapper-up">
-        <div class="c-monthyear">
-          <div class="c-month">
-            <span id="prev" class="prev fa fa-angle-left" aria-hidden="true"></span>
-            <div id="c-paginator">
-              <span class="c-paginator__month">Enero</span>
-              <span class="c-paginator__month">Febrero</span>
-              <span class="c-paginator__month">Marzo</span>
-              <span class="c-paginator__month">Abril</span>
-              <span class="c-paginator__month">Mayo</span>
-              <span class="c-paginator__month">Junio</span>
-              <span class="c-paginator__month">Julio</span>
-              <span class="c-paginator__month">Agosto</span>
-              <span class="c-paginator__month">Septiembre</span>
-              <span class="c-paginator__month">Octubre</span>
-              <span class="c-paginator__month">Noviembre</span>
-              <span class="c-paginator__month">Diciembre</span>
-              <!-- <script>
-                // 🔴 Why this keep puting the last position of the array the first of the month ???!!!!
-                monthText.forEach( month => {
-                  document.write("<span class='c-paginator__month'>"+month+"</span>");
-                });
-              </script> -->
+      <div class="wrapper">
+        <div class="d-flex wrapper-up">
+          <div class="c-monthyear">
+            <div class="c-month">
+              <span id="prev" class="prev fa fa-angle-left" aria-hidden="true"></span>
+              <div id="c-paginator">
+                <span class="c-paginator__month">Enero</span>
+                <span class="c-paginator__month">Febrero</span>
+                <span class="c-paginator__month">Marzo</span>
+                <span class="c-paginator__month">Abril</span>
+                <span class="c-paginator__month">Mayo</span>
+                <span class="c-paginator__month">Junio</span>
+                <span class="c-paginator__month">Julio</span>
+                <span class="c-paginator__month">Agosto</span>
+                <span class="c-paginator__month">Septiembre</span>
+                <span class="c-paginator__month">Octubre</span>
+                <span class="c-paginator__month">Noviembre</span>
+                <span class="c-paginator__month">Diciembre</span>
+                <!-- <script>
+                  // 🔴 Why this keep puting the last position of the array the first of the month ???!!!!
+                  monthText.forEach( month => {
+                    document.write("<span class='c-paginator__month'>"+month+"</span>");
+                  });
+                </script> -->
+              </div>
+              <span id="next" class="next fa fa-angle-right" aria-hidden="true"></span>
             </div>
-            <span id="next" class="next fa fa-angle-right" aria-hidden="true"></span>
+            <span class="c-paginator__year"></span>
           </div>
-          <span class="c-paginator__year"></span>
+          <div class="c-sort d-flex flex-wrap align-content-center">
+            <a class="o-btn c-today__btn" href="javascript:;">Hoy</a>
+          </div>
         </div>
-        <div class="c-sort">
-          <a class="o-btn c-today__btn" href="javascript:;">Hoy</a>
+        <div class="add_button">
+          <button type="button" class="btn btn-outline-primary me-2 ps-1 pe-1" data-bs-toggle="modal" data-bs-target="#newCliente">Añadir Cita</button>
         </div>
       </div>
     </nav>
+
+    <!-- Modal for add a new session -->
+    <div class="modal fade" id="newCliente" tabindex="-1" aria-labelledby="modalNewCliente" style="display: none;" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="modalNewCliente">Añadir un nuevo Cliente</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form action="#" method="post" class="row g-3" enctype="multipart/form-data">
+                  <!-- Nombre -->
+                  <div class="col-12">
+                    <label for="nombre" class="form_label">Nombre:<span class="text-danger">*</span> </label>
+                    <input type="text" name="nombre" id="nombre" required class="form-control">
+                  </div>
+                  <!-- tlf -->
+                  <div class="col-md-6">
+                    <label for="tlf" class="form_label">Tlf:<span class="text-danger">*</span> </label>
+                    <input type="tlf" name="tlf" id="tlf" required class="form-control">
+                  </div>
+                  <!-- tlf2 -->
+                  <div class="col-md-6">
+                    <label for="tlf2" class="form_label">Tlf2: </label>
+                    <input type="tlf" name="tlf2" id="tlf2" required class="form-control">
+                  </div>
+                  <div class="col-12 d-flex align-items-center justify-content-end">
+                    <button type="submit" name="newCliente" class="btn btn-secondary">Crear</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
     <div class="wrapper">
       <div class="c-calendar">
         <div class="c-calendar__style c-aside">
