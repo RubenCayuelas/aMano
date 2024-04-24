813c528607845b656a63c51d9558afe7cc7e53c1
RubenCayuelas <ruben.cayuelas@nazaries.com>
2024-04-24 14:37:08 +0200

    Changes

--- controller/users/recepcionista/controlador_citas.php
+++ controller/users/recepcionista/controlador_citas.php
@@ -34,13 +34,9 @@ if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'R' && $_SESSION['i
   
   // Body recepcionista - citas
   include('../../../view/users/recepcionista/citas/recepcionista_body.php');
-
   // include('../../../view/users/recepcionista/citas/recepcionista_bodyScript.php');
-
   // include('../../../view/users/recepcionista/citas/recepcionista_body2.php');
-
   // include('../../../view/users/recepcionista/citas/recepcionista_bodyGlobalScript.php');
-
   include('../../../view/users/recepcionista/citas/recepcionista_body_solicitudes.php');
 
   
