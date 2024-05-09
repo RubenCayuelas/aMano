02a5253b26197ded38b0054db3af3ae4f44887da
Rubén Cayuelas <ruben.cayuelas.prf@gmail.com>
2024-05-10 00:14:17 +0200

    Client Profile Update

--- controller/users/cliente/controlador_miPerfil.php
+++ controller/users/cliente/controlador_miPerfil.php
@@ -6,9 +6,20 @@ include_once('../../../model/php/funciones.php');
 session_init();
 $themeState = session_theme();
 
-
 if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'C') {
 
+  include_once('../../../assets/db/db.php');
+  include('../../../model/php/clientes.php');
+  // include('../../../model/php/fotografos.php');
+  // include('../../../model/php/servicios.php');
+  // include('../../../model/php/citas.php');
+  $clientes = new Clientes();
+  // $fotografos = new Fotografos();
+  // $servicios = new Servicios();
+  // $citas = new Citas();
+
+  $cliente = $clientes->getCliente($_SESSION['id']);
+  
   // Head
   include('../../../view/users/cliente/cliente_head.html');
 
