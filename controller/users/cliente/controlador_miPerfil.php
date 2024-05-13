f1e6f58cab411e6f9e6925b9cdb5dde9c3a7b9c8
Rubén Cayuelas <ruben.cayuelas.prf@gmail.com>
2024-05-14 00:22:04 +0200

    Cliente-MiPerfil and Trabajos Updated

--- controller/users/cliente/controlador_miPerfil.php
+++ controller/users/cliente/controlador_miPerfil.php
@@ -10,21 +10,34 @@ if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'C') {
 
   include_once('../../../assets/db/db.php');
   include('../../../model/php/clientes.php');
-  // include('../../../model/php/fotografos.php');
-  // include('../../../model/php/servicios.php');
-  // include('../../../model/php/citas.php');
+  include('../../../model/php/trabajo.php');
+  include('../../../model/php/foto.php');
+  include('../../../model/php/citas.php');
   $clientes = new Clientes();
-  // $fotografos = new Fotografos();
-  // $servicios = new Servicios();
-  // $citas = new Citas();
+  $trabajos = new Trabajo();
+  $fotos = new Foto();
+  $citas = new Citas();
 
   $cliente = $clientes->getCliente($_SESSION['id']);
+  $listaTrabajos = $trabajos->getTrabajos($_SESSION['id']);
+  
+  // $listaCitas = $citas->getAllSessionsForClient($_SESSION['id']);
+  
+  for ($i=0; $i < count($listaTrabajos); $i++) {
+    $previewTrabajosPictures[$i] = $fotos->getPreviewForTrabajo($listaTrabajos[$i]['id']);
+  }
 
   // Head
   include('../../../view/users/cliente/cliente_head.php');
 
   // Body cliente - miPerfil
   include('../../../view/users/cliente/miPerfil/cliente_body.php');
+
+  if ($listaTrabajos == null || $listaTrabajos == '') {
+    include('../../../view/users/cliente/miPerfil/bodyParts/body_no_results.html');
+  } else {
+    include('../../../view/users/cliente/miPerfil/bodyParts/body_results.php');
+  }
 
   // End
   include('../../../view/users/cliente/cliente_end.html');
