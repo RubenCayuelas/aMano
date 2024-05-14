7a064dce22a16fe5c283499ec89d98a0d9f385dd
Rubén Cayuelas <ruben.cayuelas.prf@gmail.com>
2024-05-14 23:46:24 +0200

    Changes and Fixes

--- view/users/cliente/miPerfil/cliente_body.php
+++ view/users/cliente/miPerfil/cliente_body.php
@@ -13,7 +13,7 @@
         <div class="row mb-3">
           <div class="col d-flex flex-sm-nowrap flex-wrap ">
             <div class="profile_picture m-auto m-sm-auto" data-bs-toggle="modal" data-bs-target="#profilePicture">
-              <img class="img-fluid w-15rem h-15rem" src="../../../assets/img/usersPictures/<?php echo $cliente['foto'] ?>" alt="@<?php echo $cliente['nick'] ?>">
+              <img class="img-fluid rounded-circle w-15rem h-15rem" src="../../../assets/img/usersPictures/<?php echo $cliente['foto'] ?>" alt="@<?php echo $cliente['nick'] ?>">
               <div class="edit_picture_button position-absolute color-bg-default rounded-2 color-fg-default px-2 py-1 left-0 ml-2 mb-2 border" style="top: 18rem;">
                 <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-pencil">
                   <path d="M11.013 1.427a1.75 1.75 0 0 1 2.474 0l1.086 1.086a1.75 1.75 0 0 1 0 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 0 1-.927-.928l.929-3.25c.081-.286.235-.547.445-.758l8.61-8.61Zm.176 4.823L9.75 4.81l-6.286 6.287a.253.253 0 0 0-.064.108l-.558 1.953 1.953-.558a.253.253 0 0 0 .108-.064Zm1.238-3.763a.25.25 0 0 0-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 0 0 0-.354Z"></path>
@@ -33,7 +33,7 @@
                     <form action="#" method="post" enctype="multipart/form-data">
                       <!-- Picture -->
                       <div class="col-10 m-auto">
-                        <input type="file" name="img" id="img" required class="form-control">
+                        <input type="file" name="picture" id="picture" class="form-control">
                       </div>
                       <div class="col-12 d-flex mt-3">
                         <div class="col-9 d-flex align-items-center justify-content-end pe-3">
@@ -80,7 +80,7 @@
 
                   <!-- Send -->
                   <div class="col-2 d-flex align-items-center justify-content-end">
-                    <button type="submit" class="btn btn-sm btn-outline-secondary rounded mt-4">Guardar</button>
+                    <button type="submit" name="modCliente" class="btn btn-sm btn-outline-secondary rounded mt-4">Guardar</button>
                   </div>
                 </div>
               </form>
@@ -92,7 +92,7 @@
                       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body row g-3">
-                      <form class="d-flex" action="#" method="post" enctype="multipart/form-data">
+                      <form class="d-flex" action="#" method="post">
                         <!-- Contraseña -->
                         <div class="col-9">
                           <label for="pass" class="form_label">Contraseña:<span class="text-danger">*</span> </label>
