50719a445664a1ae1472daf661bc378d953de536
Rubén Cayuelas <ruben.cayuelas.prf@gmail.com>
2024-05-23 23:06:04 +0200

    Controller update

--- controller/users/fotografo/controlador_trabajo.php
+++ controller/users/fotografo/controlador_trabajo.php
@@ -10,13 +10,25 @@ $themeState = session_theme();
 if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'F') {
   if (isset($_POST['openProyect'])) {
 
+    include_once('../../../models/php/db.php');
+    include('../../../models/php/trabajo.php');
+    include('../../../models/php/foto.php');
+    $trabajos = new Trabajo();
+    $fotos = new Foto();
+
+    // Datos de el trabajo abierto 
+    $trabajo = $trabajos->getTrabajo($_POST['trabajo_id']);
+    $listaFotos = $fotos->getPicturesForTrabajo($_POST['trabajo_id']);
+    $previewPicture = $fotos->getPreviewForTrabajo($_POST['trabajo_id']);
 
     // Head
     include('../../../view/users/fotografo/fotografo_head.php');
-  
+
     // Body fotografo - misCitas
     include('../../../view/users/fotografo/trabajos/fotografo_body.php');
-  
+
+    include('../../../view/users/fotografo/trabajos/bodyParts/body_picture_list.php');
+
     // End
     include('../../../view/users/fotografo/fotografo_end.html');
     
