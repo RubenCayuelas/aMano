28e97d834db6c2639399cc67676597f959cabfdf
Rubén Cayuelas <ruben.cayuelas.prf@gmail.com>
2024-06-02 19:31:35 +0200

    Foto model Updated new functions added

--- models/php/foto.php
+++ models/php/foto.php
@@ -11,6 +11,16 @@ class Foto
     $this->BD = BD::connect();
   }
 
+  // Get all the data from a Picture
+  public function getPicture($id)
+  {
+    $consulta = $this->BD->prepare("SELECT * FROM foto WHERE id = ?");
+    $consulta->bind_param('i', $id);
+    $consulta->execute();
+    $resultado = $consulta->get_result()->fetch_assoc();
+    return $resultado;
+  }
+
   // Obtain the Pictures for a work
   public function getPreviewForTrabajo($id_trabajo)
   {
@@ -49,4 +59,79 @@ class Foto
     $consulta->close();
     return $this->foto;
   }
+
+  // Save the pictures uploaded for a work
+  public function savePictures($id_trabajo, $fotos, $nick_usuario, $nombre_trabajo)
+  {
+    // Return if fotos is empty
+    if (empty($fotos['name'][0])) {
+      return false;
+    }
+
+    foreach ($fotos['name'] as $index => $nombreOriginal) {
+      if ($fotos['error'][$index] === UPLOAD_ERR_OK) {
+        // Verify if the file is a image
+        if (getimagesize($fotos['tmp_name'][$index]) !== false) {
+          // Get the file extension
+          $extension = pathinfo($nombreOriginal, PATHINFO_EXTENSION);
+
+          // Obtain the next id for the picture
+          $consulta = $this->BD->prepare("SHOW TABLE STATUS LIKE 'foto'");
+          $consulta->execute();
+          $resultado = $consulta->get_result();
+          $row = $resultado->fetch_assoc();
+          $nextId = $row['Auto_increment'];
+          $consulta->close();
+
+          // Create the name of the image and the route
+          $nombreImagen = $nombre_trabajo . '_' . $id_trabajo . '_' . $nextId . '.' . $extension;
+          $rutaImagen = "../../../assets/img/trabajos/$nick_usuario/$nombre_trabajo/$nombreImagen";
+
+          // Create the directory if it doesn't exist
+          if (!file_exists("../../../assets/img/trabajos/$nick_usuario/$nombre_trabajo")) {
+            mkdir("../../../assets/img/trabajos/$nick_usuario/$nombre_trabajo", 0777, true);
+          }
+
+          // Save the image into the specified directory
+          if (move_uploaded_file($fotos['tmp_name'][$index], $rutaImagen)) {
+            // Insert the new image into the database
+            $consulta = $this->BD->prepare("INSERT INTO foto (id_trabajo, foto) VALUES (?, ?)");
+            $consulta->bind_param('is', $id_trabajo, $nombreImagen);
+            $consulta->execute();
+            $consulta->close();
+          } else {
+            return false; // If the image cannot be moved
+          }
+        } else {
+          return false; // If the file is not an image
+        }
+      } else {
+        return false; // If there is an upload error
+      }
+    }
+
+    return true;
+  }
+
+  // Delete a picture
+  public function deletePicture($id_foto, $nick_usuario, $nombre_trabajo)
+  {
+    $foto = $this->getPicture($id_foto);
+
+    $nombreArchivo = $foto['foto'];
+    $rutaArchivo = "../../../assets/img/trabajos/$nick_usuario/$nombre_trabajo/$nombreArchivo";
+
+    // Delete the picture file if it exists
+    if (file_exists($rutaArchivo)) {
+      unlink($rutaArchivo);
+    }
+
+    // Delete the record from the database
+    $consulta = $this->BD->prepare("DELETE FROM foto WHERE id = ?");
+    $consulta->bind_param('i', $id_foto);
+    $consulta->execute();
+    $consulta->close();
+  }
+
+
 }
