39f83cac41efc823bf1daf25bb3bd4b758d8f5c9
Rubén Cayuelas <ruben.cayuelas.prf@gmail.com>
2024-06-02 19:32:12 +0200

    Photographer work controller update

--- controller/users/fotografo/controlador_trabajo.php
+++ controller/users/fotografo/controlador_trabajo.php
@@ -13,15 +13,20 @@ if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'F') {
     include_once('../../../models/php/db.php');
     include('../../../models/php/trabajo.php');
     include('../../../models/php/foto.php');
+    include('../../../models/php/clientes.php');
     $trabajos = new Trabajo();
     $fotos = new Foto();
+    $clientes = new Clientes();
 
-    // $trabajo_id = $_POST['trabajo_id'];
+    $trabajo_id = $_POST['trabajo_id'];
 
     // Manejo de la actualización del estado del proyecto vía AJAX
-    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['updateWorkStatus'])) {
+    // Fix it's not working
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
       $input = json_decode(file_get_contents('php://input'), true);
       if (isset($input['publico']) && isset($input['trabajo_id'])) {
+
         $publico = $input['publico'] ? 1 : 0;
         $trabajo_id = $input['trabajo_id'];
         $trabajos->updateWorkPublicStatus($trabajo_id, $publico);
@@ -34,10 +39,23 @@ if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'F') {
       }
     }
 
+    if (isset($_POST['newPictures'])) {
+      $datos_trabajo = $trabajos->getTrabajo($trabajo_id);
+      $datos_usuario = $clientes->getCliente($datos_trabajo['id_cliente']);
+      
+      $result = $fotos->savePictures($trabajo_id, $_FILES['pictures'], $datos_usuario['nick'], $datos_trabajo['nombre']);
+
+    } elseif (isset($_POST['deletePicture'])) {
+      $datos_trabajo = $trabajos->getTrabajo($trabajo_id);
+      $datos_usuario = $clientes->getCliente($datos_trabajo['id_cliente']);
+
+      $fotos->deletePicture($_POST['deletePicture'], $datos_usuario['nick'], $datos_trabajo['nombre']);
+    }
+
     // Datos de el trabajo abierto 
-    $trabajo = $trabajos->getTrabajo($_POST['trabajo_id']);
-    $listaFotos = $fotos->getPicturesForTrabajo($_POST['trabajo_id']);
-    $previewPicture = $fotos->getPreviewForTrabajo($_POST['trabajo_id']);
+    $trabajo = $trabajos->getTrabajo($trabajo_id);
+    $listaFotos = $fotos->getPicturesForTrabajo($trabajo_id);
+    $previewPicture = $fotos->getPreviewForTrabajo($trabajo_id);
 
     // Head
     include('../../../view/users/fotografo/fotografo_head.php');
@@ -45,11 +63,11 @@ if (isset($_SESSION['userType']) && $_SESSION['userType'] == 'F') {
     // Body fotografo - misCitas
     include('../../../view/users/fotografo/trabajos/fotografo_body.php');
 
-    // if ($listaFotos != []) {
-      // include('../../../view/users/fotografo/trabajos/bodyParts/body_picture_list.php');
-    // } else {
-    //   include('../../../view/users/fotografo/trabajos/bodyParts/body_no_results.html');
-    // }
+    if ($listaFotos != []) {
+      include('../../../view/users/fotografo/trabajos/bodyParts/body_picture_list.php');
+    } else {
+      include('../../../view/users/fotografo/trabajos/bodyParts/body_no_results.html');
+    }
 
     // End
     include('../../../view/users/fotografo/fotografo_end.html');
